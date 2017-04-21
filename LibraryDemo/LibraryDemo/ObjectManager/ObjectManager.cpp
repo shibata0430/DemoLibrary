0bc2302e27ca5322a6696903a7b9e0d4bdea7f5e
s-2013000001366\share <shibatatetsuo0430@gmail.com>
2017-04-21 16:45:16 +0900

    全体を管理するクラスのついか

---
+++ LibraryDemo/LibraryDemo/ObjectManager/ObjectManager.cpp
@@ -0,0 +1,37 @@
+﻿/**
+* @file		ObjectManager.cpp
+* @breif	出てくるオブジェクトを管理しているクラス実装
+* @author	shibata
+*/
+
+#include "ObjectManager.h"
+#include "../Character/CharacterManager.h"
+#include "../Field/FieldManager.h"
+#include <Library\DebugSystem.h>
+#include <Library\Define.h>
+
+
+ObjectManager::ObjectManager() : 
+m_pCharacterManager(New CharacterManager),
+m_pFieldManager(New FieldManager)
+{
+	MyAssert(m_pCharacterManager, "NULLが入っています");
+	MyAssert(m_pFieldManager, "NULLが入っています");
+}
+
+ObjectManager::~ObjectManager()
+{
+	SafeDelete(m_pCharacterManager);
+	SafeDelete(m_pFieldManager);
+}
+
+void ObjectManager::Control()
+{
+	m_pCharacterManager->Control();
+}
+
+void ObjectManager::Draw()
+{
+	m_pFieldManager->Drwa();
+	m_pCharacterManager->Draw();
+}
---
+++ LibraryDemo/LibraryDemo/ObjectManager/ObjectManager.h
@@ -0,0 +1,39 @@
+﻿/**
+ * @file	ObjectManager.h
+ * @breif	出てくるオブジェクトを管理しているクラスヘッダ
+ * @author	shibata
+ */
+
+#ifndef OBJECTMANAGER_H
+#define OBJECTMANAGER_H
+
+class CharacterManager;
+class FieldManager;
+
+class ObjectManager
+{
+public:
+	enum MeshKind
+	{
+		PLAYER,
+		CHIPS,
+		FLOOR,
+	};
+
+public:
+
+	/**コンストラクタ*/
+	ObjectManager();
+	/**デストラクタ*/
+	~ObjectManager();
+	/**操作関数*/
+	void Control();
+
+	void Draw();
+
+private:
+	CharacterManager*	m_pCharacterManager;
+	FieldManager*		m_pFieldManager;
+};
+
+#endif // !OBJECTMANAGER_H
