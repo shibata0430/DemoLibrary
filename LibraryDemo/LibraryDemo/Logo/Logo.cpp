668d0e8935aa13f38df0de1b8a6ab036ecf72348
ガレリア\哲良 <shibatatetsuo0430@gmail.com>
2017-04-24 05:17:30 +0900

    汎用的なロゴクラスを作成

---
+++ LibraryDemo/LibraryDemo/Logo/Logo.cpp
@@ -0,0 +1,24 @@
+﻿#include "Logo.h"
+#include <Library.h>
+
+Logo::Logo(float width_, float height_, float posX_, float posY_, int texindex_, const char* pFilePath_) :
+m_rLibrary(Library::Instance()),
+m_posX(posX_),
+m_posY(posY_),
+m_width(width_),
+m_height(height_),
+m_texindex(texindex_),
+m_pFilePath(pFilePath_)
+{
+	m_rLibrary.LoadTextureFile(m_texindex, m_pFilePath);
+	m_rLibrary.SetTexSize(m_texindex, m_width, m_height);
+}
+
+Logo::~Logo()
+{
+}
+
+void Logo::Draw()
+{
+	m_rLibrary.DrawCenter(m_texindex, m_posX, m_posY);
+}
---
+++ LibraryDemo/LibraryDemo/Logo/Logo.h
@@ -0,0 +1,41 @@
+﻿/**
+ * @file	Logo.h
+ * @breif	フリー素材のロゴを表示するクラスヘッダ
+ * @author	shibata
+ */
+
+#ifndef LOGO_H
+#define LOGO_H
+
+class Library;
+
+class Logo
+{
+public:
+	/**
+	 * 色んなロゴを描画するクラスのコンストラクタ
+	 * @param [in] width_		ロゴの描画時の幅
+	 * @param [in] height_		ロゴの描画時の高さ
+	 * @param [in] posX_		ロゴの表示するX座標
+	 * @param [in] posY_		ロゴの表示するY座標
+	 * @param [in] texindex_	ロゴのID番号
+	 * @param [in] pFilePath_	読み込むロゴのファイルパス
+	 */
+	Logo(float width_, float height_, float posX_, float posY_,int texindex_, const char* pFilePath_);
+	/**デストラクタ*/
+	~Logo(); 
+
+	/**描画関数*/
+	void Draw();
+
+private:
+	Library&	m_rLibrary;
+	float		m_width;		// ロゴの描画時の幅を格納
+	float		m_height;		// ロゴの描画時の高さを格納
+	float		m_posX;			// ロゴの表示するX座標を格納
+	float		m_posY;			// ロゴの表示するY座標を格納
+	int			m_texindex;		// ロゴのID番号を格納
+	const char* m_pFilePath;	// 読み込むロゴのファイルパスを格納
+};
+
+#endif // !LOGO_H
