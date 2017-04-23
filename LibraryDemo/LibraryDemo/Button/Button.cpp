76b84e4e7003f88928d076a7dbb3641393310732
ガレリア\哲良 <shibatatetsuo0430@gmail.com>
2017-04-24 05:16:18 +0900

    ボタンクラスの作成

--- LibraryDemo/LibraryDemo/Button/Button.cpp
+++ LibraryDemo/LibraryDemo/Button/Button.cpp
@@ -1,9 +1,33 @@
 ﻿#include "Button.h"
+#include <Library.h>
 
-Button::Button(int posX_, int posY_)
+Button::Button(float posX_, float posY_, int texindex_) : 
+m_rLibrary(Library::Instance()),
+m_width(64.0f),
+m_height(64.0f),
+m_posX(posX_),
+m_posY(posY_),
+m_texIndex(texindex_)
 {
+	m_rLibrary.LoadTextureFile(m_texIndex, "Resource/Texture/Button.png");
+	m_rLibrary.SetTexSize(m_texIndex, m_width, m_height);
 }
 
 Button::~Button()
 {
+}
+
+bool Button::IsPush()
+{
+	if (m_rLibrary.CheckKey(DIK_A) == PUSH)
+	{
+		return true;
+	}
+
+	return false;
+}
+
+void Button::Draw()
+{
+	m_rLibrary.DrawCenter(m_texIndex, m_posX, m_posY);
 }
--- LibraryDemo/LibraryDemo/Button/Button.h
+++ LibraryDemo/LibraryDemo/Button/Button.h
@@ -7,21 +7,37 @@
 #ifndef BUTTON_H
 #define BUTTON_H
 
+class Library;
+
 class Button
 {
 public:
 	/**
 	 * コンストラクタ
-	 * @param [in] posX_	Ｘ軸の表示位置
-	 * @param [in] posY_	Ｙ軸の表示位置
+	 * @param [in] posX_		Ｘ軸の表示位置
+	 * @param [in] posY_		Ｙ軸の表示位置
+	 * @param [in] texindex_	ボタンのID番号
 	 */
-	Button(int posX_, int posY_);
+	Button(float posX_, float posY_, int texindex_);
 	/**デストラクタ*/
 	~Button();
 
+	/**
+	 * ボタンが押されたがどうかの関数
+	 * @retval true		押された
+	 * @retval false	押されてない
+	 */
+	bool IsPush();
+	/**描画関数*/
+	void Draw();
+
 private:
-	int m_buttonPosX;	// ボタンのＸの表示位置
-	int m_butoonPosY;	// ボタンのＹの表示位置
+	Library&	m_rLibrary;	
+	float		m_width;		// ボタンの描画時の幅
+	float		m_height;		// ボタンの描画時の高さ
+	float		m_posX;			// ボタンのX軸の位置
+	float		m_posY;			// ボタンのY軸の位置
+	int			m_texIndex;		// ボタンを描画するときのID番号
 };
 
 #endif // !BUTTON_H
